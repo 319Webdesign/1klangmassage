e268085e49b8c37e120b94695ca1e890865ff2e1
319Webdesign <schmidtmaik1909@web.de>
2026-04-16 10:00:29 +0200

    s

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,10 +1,20 @@
 import { createImageUrlBuilder, type SanityImageSource } from '@sanity/image-url'
 
-import { dataset, projectId } from '../env'
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
 // https://www.sanity.io/docs/image-url
-const builder = createImageUrlBuilder({ projectId, dataset })
+const builder =
+  projectId && dataset
+    ? createImageUrlBuilder({ projectId, dataset })
+    : null
 
 export const urlFor = (source: SanityImageSource) => {
+  if (!builder) {
+    throw new Error(
+      'Sanity image builder is not configured. Missing NEXT_PUBLIC_SANITY_PROJECT_ID or NEXT_PUBLIC_SANITY_DATASET.'
+    )
+  }
+
   return builder.image(source)
 }
